5042b6d320611ebb86a50269cf1f9f9bd487e4ba
James Morgan <ja.morgan1@outlook.com>
2022-09-30 15:37:40 +0800

    allow terraform-service SA impersonation

--- main.tf
+++ main.tf
@@ -8,8 +8,27 @@ terraform {
   }
 }
 
+locals {
+  terraform_service_account = "terraform-service@${var.project_id}.iam.gserviceaccount.com"
+}
+
 provider "google" {
-  project = var.project_id
-  region = var.region
-  impersonate_service_account = "terraform-service@${var.project_id}.iam.gserviceaccount.com"
+  project         = var.project_id
+  access_token    = data.google_service_account_access_token.default.access_token
+  request_timeout = "60s"
+}
+
+provider "google" {
+  alias = "impersonation"
+  scopes = [
+    "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/userinfo.email",
+  ]
+}
+
+data "google_service_account_access_token" "default" {
+  provider               = google.impersonation
+  target_service_account = local.terraform_service_account
+  scopes                 = ["userinfo-email", "cloud-platform"]
+  lifetime               = "1200s"
 }
