0260514a9ad3b6443940f43ac0d3c6c68cbe0377
James Morgan <ja.morgan1@outlook.com>
2022-09-30 21:41:13 +0800

    goodwe variables

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,4 @@
 project_id = "servian-u-practice"
 region     = "australia-southeast1"
+
+GCS_BUCKET_NAME   = "solar-storage-service"
--- variables.tf
+++ variables.tf
@@ -7,3 +7,19 @@ variable "region" {
   type        = string
   description = "GCP Region"
 }
+
+# variable "GOODWE_USER" {
+#   type        = string
+#   description = "GoodWe SEMS Portal Username"
+# }
+
+# variable "GOODWE_STATION_ID" {
+#   type        = string
+#   description = "GoodWe API Solar Array ID"
+# }
+
+variable "GCS_BUCKET_NAME" {
+  type        = string
+  description = "Destination Bucket for solar data"
+
+}
